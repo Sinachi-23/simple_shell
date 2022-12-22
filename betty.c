eb8d24e6f5198612fad6febc06791f7523e62f29
Ruth Ikotun <itzsinachi@gmail.com>
2022-12-22 01:03:43 -0800

    betty linter

---
+++ betty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * factorial - calculates the factorial of numbers
+ * @n: the numbrer to be factorialized
+ * Return: the factorial value
+ */
+unsigned int factorial(unsigned int n)
+{
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
